989a84f3c32b0b0eb0d272de5fb56d2f5c949619
kdh2465 <45729773+kdh2465@users.noreply.github.com>
2020-09-21 13:25:53 +0900

    코드 일부 변경

--- src/pack01_basicsyntax/EX01_JavaBasicStructure/EX01_JavaBasicStructure.java
+++ src/pack01_basicsyntax/EX01_JavaBasicStructure/EX01_JavaBasicStructure.java
@@ -8,7 +8,7 @@ public class EX01_JavaBasicStructure {
 	public static void main(String[] args) {
 		
 		//한줄주석 : 화면 출력코드
-		System.out.println("콘솔 화면 출력");
+		System.out.println("콘솔 화면에 출력");
 		
 	}
 }
